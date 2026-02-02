b9928420175a4e60085e76229d7fdd46c2aa087f
Vision-Expo <gorulevishal984@gmail.com>
2026-02-02 17:10:23 +0530

    chore(utils): add centralized logger utility

--- src/samvidai/utils/logger.py
+++ src/samvidai/utils/logger.py
@@ -1,1 +1,19 @@
-# TODO: Implement
+import logging
+
+
+def get_logger(name: str = "samvidai") -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger  # avoid duplicate handlers
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] %(name)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
